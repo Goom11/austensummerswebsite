124d549a6d1c005be304886cf2b63e96f10dcf1c
Aman <amanaamazing@gmail.com>
2021-02-24 13:14:48 -0500

    prettify app.js file

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-var fs = require('fs'),
-    http = require('http'),
-    https = require('https'),
-    express = require('express'),
-    router = require("./routes/index");
+var fs = require("fs"),
+  http = require("http"),
+  https = require("https"),
+  express = require("express"),
+  router = require("./routes/index");
 
 //App
 var app = express();
@@ -10,32 +10,33 @@ app.use(express.static("public"));
 
 //Enforce HTTPS
 app.use(function (req, res, next) {
-        if (req.secure) {
-                next();
-        } else {
-                res.redirect('https://' + req.headers.host + req.url);
-        }
+  if (req.secure) {
+    next();
+  } else {
+    res.redirect("https://" + req.headers.host + req.url);
+  }
 });
-
 
 //Routing
 app.use("/", router);
 
 //SSL Setup
 const httpServer = http.createServer(app);
-const httpsServer = https.createServer({
-  key: fs.readFileSync('/etc/letsencrypt/live/austensummers.com/privkey.pem'),
-  cert: fs.readFileSync('/etc/letsencrypt/live/austensummers.com/fullchain.pem'),
-}, app);
-
+const httpsServer = https.createServer(
+  {
+    key: fs.readFileSync("/etc/letsencrypt/live/austensummers.com/privkey.pem"),
+    cert: fs.readFileSync(
+      "/etc/letsencrypt/live/austensummers.com/fullchain.pem"
+    ),
+  },
+  app
+);
 
 //Launch!
 httpServer.listen(80, () => {
-    console.log('HTTP Server running on port 80!');
+  console.log("HTTP Server running on port 80!");
 });
 
 httpsServer.listen(443, () => {
-    console.log('HTTPS Server running on port 443!');
+  console.log("HTTPS Server running on port 443!");
 });
-
-
