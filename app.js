4736190fc4e5cfe4697915e7cfce2448499cda85
Ertuğrul CÖRE <ertugrulcore@gmail.com>
2021-02-24 17:43:35 +0300

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var fs = require('fs'),
     express = require('express'),
     router = require("./routes/index");
 
-var port = 8000;
+var port = 80;
 
 var options = {
     key: fs.readFileSync('/etc/letsencrypt/live/austensummers.com/privkey.pem'),
